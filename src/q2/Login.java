145c2de6b4af8ba64acec87ed284612fc6393433
Kashish Noor <f223628@cfd.nu.edu.pk>
2024-11-14 11:44:57 +0500

    Commit 2 Experimnetal Changes

--- src/q2/Login.java
+++ src/q2/Login.java
@@ -2,9 +2,12 @@ package q2;
 
 public class Login
 {
-public boolean authenticate(String username, String password) 
-{
-	System.out.println("Authenticating user...");
-return username.equals("admin") && password.equals("password123");
-}
+	public boolean authenticate(String username, String password) {
+		if (username == null || password == null) {
+		System.out.println("Invalid input");
+		return false;
+		}
+		System.out.println("Authenticating user...");
+		return username.equals("admin") && password.equals("password123");
+		}
 }
