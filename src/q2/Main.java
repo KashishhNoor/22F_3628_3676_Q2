f28cf41120c8be64189c086bd9fab0288984844d
Kashish Noor <f223628@cfd.nu.edu.pk>
2024-11-14 12:23:33 +0500

    New changes in Main

--- src/q2/Main.java
+++ src/q2/Main.java
@@ -2,11 +2,6 @@ package q2;
 
 public class Main {
 public static void main(String[] args) {
-System.out.println("Welcome to the application!");
-greetUser();
-}
-
-public static void greetUser() {
-System.out.println("Hello, User!");
+System.out.println("Feature A work in progress.");
 }
 }
