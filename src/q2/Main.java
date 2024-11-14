c32c6e9ab2af16b3af128a5b47ded701722f14e0
Kashish Noor <f223628@cfd.nu.edu.pk>
2024-11-14 12:25:48 +0500

    New changes in Main

--- src/q2/Main.java
+++ src/q2/Main.java
@@ -1,7 +1,13 @@
 package q2;
 
 public class Main {
-public static void main(String[] args) {
-System.out.println("Feature A work in progress.");
-}
+    public static void main(String[] args) {
+        System.out.println("Feature A work in progress.");
+        System.out.println("Welcome to the application of Github!");
+        greetUser();
+    }
+
+    public static void greetUser() {
+        System.out.println("Hello, User123!");
+    }
 }
